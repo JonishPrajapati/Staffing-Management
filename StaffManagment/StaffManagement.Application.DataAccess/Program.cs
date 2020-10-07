8d8d2b9112015405388a2f7b3f2134ef3c239647
hsinoj1996 <hsinojjonish@gmail.com>
2020-10-07 09:03:35 -0800

    Initiliazing Staffing company

---
+++ StaffManagment/StaffManagement.Application.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaffManagement.Application.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
